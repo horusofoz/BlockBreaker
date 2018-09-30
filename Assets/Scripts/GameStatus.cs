6dfb6797fe7689f7beb47f92fde8c1a788920300
Jeff <horusofoz@gmail.com>
2018-09-30 12:46:45 +0930

    Tutorial 23 - Using Time.timeScale * Set default game speed to .85 * Set game speed to serializable for debugging/testing

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour {
+
+    // Config Parameters
+    [Range(0.1f, 1.5f)] [SerializeField] float gameSpeed = 0.85f;
+
+	// Use this for initialization
+	void Start () 
+	{
+        Time.timeScale = gameSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        gameSpeed = Time.timeScale;
+    }
+}
