0296279aaeac688e88e5d4028d5e561d99f20c03
Jeff <horusofoz@gmail.com>
2018-10-02 16:21:51 +0930

    Tutorial 36 - Preventing boring ball loops * Added random factor of 0 to .2 to velocity on collisions to prevent ball bounciung perfectly horizontally or vertically

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
@@ -10,6 +7,7 @@ public class Ball : MonoBehaviour {
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
     [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float randomFactor = 0.2f;
 
     // State
     Vector2 paddleToBallVector;
@@ -17,12 +15,14 @@ public class Ball : MonoBehaviour {
 
     // Cached Component References
     AudioSource myAudioSource;
+    Rigidbody2D myRigidbody;
 
 	// Use this for initialization
 	void Start () 
 	{
         paddleToBallVector = transform.position - paddle1.transform.position;
         myAudioSource = GetComponent<AudioSource>();
+        myRigidbody = GetComponent<Rigidbody2D>();
 
     }
 	
@@ -42,7 +42,7 @@ public class Ball : MonoBehaviour {
         if(Input.GetMouseButtonDown(0))
         {
             hasStarted = true;
-            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            myRigidbody.velocity = new Vector2(xPush, yPush);
         }
     }
 
@@ -54,10 +54,15 @@ public class Ball : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Vector2 velocityTweak = new Vector2
+            (Random.Range(0f, randomFactor),
+            Random.Range(0f, randomFactor));
+
         if(hasStarted)
         {
-            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            AudioClip clip = ballSounds[Random.Range(0, ballSounds.Length)];
             myAudioSource.PlayOneShot(clip);
+            myRigidbody.velocity += velocityTweak;
         }
     }
 }
